aeea86ba68bcd97ebbd339b40b93da7f9de5ec22
Alumona Benaiah <65370456+Benrobo@users.noreply.github.com>
2020-06-30 23:19:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+function opennav(){ document.getElementById("sidebar").style.width="200px"
+}
+
+
+function closenav(){ document.getElementById("sidebar").style.width="0px"
+}
+
+function  showside(){
+    document.getElementById ("drbtn").style.visibility = "visible";
+    document.getElementById ("drbtn").style.transition = ".5s";
+}
+
+function  hideside(){
+    document.getElementById ("drbtn").style.visibility = "hidden";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
